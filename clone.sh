bc8e5c6189a4bf40a099f9aa55c4315496fa9f61
LAHleo <43699874+LAHleo@users.noreply.github.com>
2019-02-13 06:02:41 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,9 +5,9 @@ read -p "虚拟机名字:"  A
 #根据原始盘创建磁盘文件
 qemu-img create -b /var/lib/libvirt/images/node.qcow2  -f qcow2  /var/lib/libvirt/images/$A.img  20G    
 
-cp node.xml  /etc/libvirt/qemu/$A.xml
-cp vbr.xml  /etc/libvirt/qemu/networks/vbr.xml
-#
+\cp node.xml  /etc/libvirt/qemu/$A.xml
+\cp vbr.xml  /etc/libvirt/qemu/networks/vbr.xml
+#创建虚拟交换机
 virsh net-define  /etc/libvirt/qemu/networks/vbr.xml
 virsh net-start vbr
 virsh net-autostart vbr
