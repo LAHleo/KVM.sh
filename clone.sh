366133ec2992d6784ff46f529bdf180ad9534424
LAHleo <43699874+LAHleo@users.noreply.github.com>
2019-02-13 05:50:04 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,11 +2,14 @@
 
 read -p "虚拟机名字:"  A
 
-#原始盘
+#根据原始盘创建磁盘文件
 qemu-img create -b /var/lib/libvirt/images/node.qcow2  -f qcow2  /var/lib/libvirt/images/$A.img  20G    
+
 cp /var/ftp/node.xml  /etc/libvirt/qemu/$A.xml
+
 sed -i  "2s/node/$A/" /etc/libvirt/qemu/$A.xml
 sed -i  "26s/node.img/$A.img/" /etc/libvirt/qemu/$A.xml
+#创建虚拟机
 virsh  define  /etc/libvirt/qemu/$A.xml
 virsh  start  $A
 
